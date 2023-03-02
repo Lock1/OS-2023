a00ac2341b485c85c9717c81085a47aa5e5aacdc
Lock1 <rizaltanur@outlook.com>
2023-03-03 05:49:24 +0700

    feat: PIC constant extras

--- src/interrupt.c
+++ src/interrupt.c
@@ -25,8 +25,8 @@ void pic_ack(uint8_t irq) {
 }
 
 void activate_keyboard_interrupt(void) {
-    out(PIC1_COMMAND + IRQ_KEYBOARD, 0xFD);
-    out(0xA1, 0xFF);
+    out(PIC1_DATA, PIC_DISABLE_ALL_MASK ^ (1 << IRQ_KEYBOARD));
+    out(PIC2_DATA, PIC_DISABLE_ALL_MASK);
 }
 
 void pic_remap(void) {
--- src/lib-header/interrupt.h
+++ src/lib-header/interrupt.h
@@ -4,35 +4,58 @@
 #include "stdtype.h"
 
 /* -- PIC constants -- */
-#define PIC1_OFFSET     0x20
-#define PIC2_OFFSET     0x28
 
-#define PIC1            0x20
-#define PIC2            0xA0
-#define PIC1_COMMAND    PIC1
-#define PIC1_DATA       (PIC1 + 1)
-#define PIC2_COMMAND    PIC2
-#define PIC2_DATA       (PIC2 + 1)
+// PIC interrupt offset
+#define PIC1_OFFSET          0x20
+#define PIC2_OFFSET          0x28
 
-#define PIC_ACK         0x20
+// PIC ports
+#define PIC1                 0x20
+#define PIC2                 0xA0
+#define PIC1_COMMAND         PIC1
+#define PIC1_DATA            (PIC1 + 1)
+#define PIC2_COMMAND         PIC2
+#define PIC2_DATA            (PIC2 + 1)
 
-#define ICW1_ICW4       0x01   /* ICW4 (not) needed */
-#define ICW1_SINGLE     0x02   /* Single (cascade) mode */
-#define ICW1_INTERVAL4  0x04   /* Call address interval 4 (8) */
-#define ICW1_LEVEL      0x08   /* Level triggered (edge) mode */
-#define ICW1_INIT       0x10   /* Initialization - required! */
+// PIC ACK & mask constant
+#define PIC_ACK              0x20
+#define PIC_DISABLE_ALL_MASK 0xFF
 
-#define ICW4_8086       0x01   /* 8086/88 (MCS-80/85) mode */
-#define ICW4_AUTO       0x02   /* Auto (normal) EOI */
-#define ICW4_BUF_SLAVE  0x08   /* Buffered mode/slave */
-#define ICW4_BUF_MASTER 0x0C   /* Buffered mode/master */
-#define ICW4_SFNM       0x10   /* Special fully nested (not) */
+// PIC remap constants
+#define ICW1_ICW4            0x01   /* ICW4 (not) needed */
+#define ICW1_SINGLE          0x02   /* Single (cascade) mode */
+#define ICW1_INTERVAL4       0x04   /* Call address interval 4 (8) */
+#define ICW1_LEVEL           0x08   /* Level triggered (edge) mode */
+#define ICW1_INIT            0x10   /* Initialization - required! */
+
+#define ICW4_8086            0x01   /* 8086/88 (MCS-80/85) mode */
+#define ICW4_AUTO            0x02   /* Auto (normal) EOI */
+#define ICW4_BUF_SLAVE       0x08   /* Buffered mode/slave */
+#define ICW4_BUF_MASTER      0x0C   /* Buffered mode/master */
+#define ICW4_SFNM            0x10   /* Special fully nested (not) */
+
 
 /* -- PICs IRQ list -- */
-#define IRQ_TIMER       0
-#define IRQ_KEYBOARD    1
 
+// PIC Master
+#define IRQ_TIMER        0
+#define IRQ_KEYBOARD     1
+#define IRQ_CASCADE      2
+#define IRQ_COM2         3
+#define IRQ_COM1         4
+#define IRQ_LPT2         5
+#define IRQ_FLOPPY_DISK  6
+#define IRQ_LPT1_SPUR    7
 
+// PIC Slave
+#define IRQ_CMOS         8
+#define IRQ_PERIPHERAL_1 9
+#define IRQ_PERIPHERAL_2 10
+#define IRQ_PERIPHERAL_3 11
+#define IRQ_MOUSE        12
+#define IRQ_FPU          13
+#define IRQ_PRIMARY_ATA  14
+#define IRQ_SECOND_ATA   15
 
 
 /**
