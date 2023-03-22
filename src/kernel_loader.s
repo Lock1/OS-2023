c4e004119f19fa226d91b07d3a468bc3bf208663
Lock1 <rizaltanur@outlook.com>
2023-03-23 01:36:48 +0700

    feat: Unclean but seems PE PG

--- src/kernel_loader.s
+++ src/kernel_loader.s
@@ -1,15 +1,17 @@
-global loader                                 ; the entry symbol for ELF
+global low_loader                             ; the entry symbol for ELF
 global enter_protected_mode                   ; go to protected mode
 extern kernel_setup                           ; kernel
+extern _gdt_gdtr
 extern _paging_kernel_page_directory          ; kernel page directory
 extern _kernel_start
 extern _kernel_end
 
-KERNEL_STACK_SIZE equ 4194304                 ; size of stack in bytes
-MAGIC_NUMBER      equ 0x1BADB002              ; define the magic number constant
-FLAGS             equ 0x0                     ; multiboot flags
-CHECKSUM          equ -MAGIC_NUMBER           ; calculate the checksum
-                                              ; (magic number + checksum + flags should equal 0)
+KERNEL_VIRTUAL_BASE equ 0xC0000000
+KERNEL_STACK_SIZE   equ 4194304                 ; size of stack in bytes
+MAGIC_NUMBER        equ 0x1BADB002              ; define the magic number constant
+FLAGS               equ 0x0                     ; multiboot flags
+CHECKSUM            equ -MAGIC_NUMBER           ; calculate the checksum
+                                                ; (magic number + checksum + flags should equal 0)
 
 section .bss
 align 4                                       ; align at 4 bytes
@@ -22,121 +24,46 @@ align 4                                       ; the code must be 4 byte aligned
     dd FLAGS                                  ; the flags,
     dd CHECKSUM                               ; and the checksum
 
+section .data
+align 4096
+global TEMP_PAGE_DIRECTORY
+TEMP_PAGE_DIRECTORY:
+    ; Map the first 4mb physical memory to first 4mb virtual memory. Otherwise, when paging is enabled, eip points to, 0x100004 for example, and MMU is not gonna know how to translate 
+    ; this address into phsyical mem address, because our PDE doesn't tell MMU how to find it.
+    dd 0x00000083
+    times(0x300 - 1) dd 0
+    dd 0x00000083
+    times(1024 - 0x300 - 1) dd 0 
 
 section .setup.text                           ; start of the text (code) section
-loader:                                       ; the loader label (defined as entry point in linker script)
-    mov esp, kernel_stack + KERNEL_STACK_SIZE ; setup stack register to proper location
-    
-    ; Setup paging
-    mov edi, _paging_kernel_page_directory - 0xC0000000
+low_loader equ (loader - KERNEL_VIRTUAL_BASE)
+loader:                                   ; the loader label (defined as entry point in linker script)
+setup_paging:
+    mov edi, TEMP_PAGE_DIRECTORY - KERNEL_VIRTUAL_BASE
     mov esi, 0
-
+    
     ; First 4 MB
     mov edx, esi
-    or  edx, 0x43           ; Present writable flag
+    or  edx, 0x83           ; Present writable flag
     mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Second 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Second 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Second 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Second 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Sixth 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-
-
-
-
 
 ;   Higher half mapping
-    mov esi, 0
     ; on page directory corresponding 0xC000 0000, map those into physical address starting from 0
-    mov edi, _paging_kernel_page_directory - 0xC0000000 + 4 * 0x300 
+    mov edi, TEMP_PAGE_DIRECTORY - KERNEL_VIRTUAL_BASE + 4 * 0x300 
+    mov esi, 0
 
     ; First 4 MB
     mov edx, esi
-    or  edx, 0x43           ; Present writable flag
+    or  edx, 0x83           ; Present writable flag
     mov [edi], edx
 
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Second 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Second 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Second 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Second 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
-
-    add esi, 4*1024*1024
-    add edi, 4
-
-    ; Sixth 4 MB
-    mov edx, esi
-    or  edx, 0x43           ; Present writable flag
-    mov [edi], edx
 
 
     ; Identity mapping
 ; paging_init_loop_1:
 ;     cmp [_kernel_start], esi
 ;     jl  paging_init_loop_2
-;     cmp [_kernel_end - 0xC0000000], esi
+;     cmp [_kernel_end - KERNEL_VIRTUAL_BASE], esi
 ;     jge paging_init_loop_3
 
 ;     ; Get and set page directory entry
@@ -150,37 +77,38 @@ loader:                                       ; the loader label (defined as ent
 ;     loop paging_init_loop_1
 
 ; paging_init_loop_3:
-    ; Map first 4 MB (including framebuffer) as present and writable
-    mov  ecx, 0x43
-    mov [_paging_kernel_page_directory - 0xC0000000 + 0x300 * 4], ecx
 
     ; Set CR3 (CPU page register)
-    mov ecx, _paging_kernel_page_directory - 0xC0000000
-    mov cr3, ecx
+    mov eax, TEMP_PAGE_DIRECTORY - KERNEL_VIRTUAL_BASE
+    mov cr3, eax
 
     ; Use 4 MB paging
-    mov ecx, cr4
-    or  ecx, 0x00000010    ; PSE (4 MB paging)
-    mov cr4, ecx
-    ; hlt
+    mov eax, cr4
+    or  eax, 0x00000010    ; PSE (4 MB paging)
+    mov cr4, eax
+
     ; Enable paging
-    mov ecx, cr0
-    or  ecx, 0x80000000    ; PG flag
-    mov cr0, ecx
+    mov eax, cr0
+    ; mov ecx, 0xDEADBEEF
+    ; hlt
+    or  eax, 0x80000000   ; PG flag
+    mov cr0, eax
+    mov eax, 0xDEADBEEF
     hlt
-    
-    call kernel_setup
+
+    mov esp, kernel_stack + KERNEL_STACK_SIZE ; setup stack register to proper location
+    ; call kernel_setup
 .loop:
     jmp .loop                                 ; loop forever
 
-section .text
+
 ; More details: https://en.wikibooks.org/wiki/X86_Assembly/Protected_Mode
 enter_protected_mode:
     ; Load GDT from GDTDescriptor
     cli
-    mov  eax, [esp+4]
+    mov  eax, _gdt_gdtr - KERNEL_VIRTUAL_BASE
     lgdt [eax]
-
+    
     ; Set Protection Enable bit-flag in Control Register 0 (CR0)
     ; Or in other words: Switch to protected mode
     mov  eax, cr0
@@ -196,5 +124,3 @@ flush_cs:
     mov ss, ax
     mov ds, ax
     mov es, ax
-
-    ret
