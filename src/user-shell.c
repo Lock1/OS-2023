20891ebbf3bbdd1f506704aec1d2bac285b6c033
Lock1 <rizaltanur@outlook.com>
2023-03-25 07:20:17 +0700

    feat: Quick, mockup shell

--- src/user-shell.c
+++ src/user-shell.c
@@ -1,6 +1,25 @@
 #include "lib-header/stdtype.h"
 #include "lib-header/fat32.h"
 
+#define BIOS_BLACK         0x0
+#define BIOS_BLUE          0x1
+#define BIOS_GREEN         0x2
+#define BIOS_CYAN          0x3
+#define BIOS_RED           0x4
+#define BIOS_MAGENTA       0x5
+#define BIOS_BROWN         0x6
+#define BIOS_GRAY          0x7
+
+#define BIOS_DARK_GRAY     0x8
+#define BIOS_LIGHT_BLUE    0x9
+#define BIOS_LIGHT_GREEN   0xA
+#define BIOS_LIGHT_CYAN    0xB
+#define BIOS_LIGHT_RED     0xC
+#define BIOS_LIGHT_MAGENTA 0xD
+#define BIOS_YELLOW        0xE
+#define BIOS_WHITE         0xF
+
+
 void interrupt(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) {
     __asm__ volatile("mov %0, %%ebx" : /* <Empty> */ : "r"(ebx));
     __asm__ volatile("mov %0, %%ecx" : /* <Empty> */ : "r"(ecx));
@@ -10,24 +29,25 @@ void interrupt(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) {
     __asm__ volatile("int $0x30");
 }
 
-int main(void) {
-    struct ClusterBuffer cl = {0};
-    struct FAT32DriverRequest request = {
-        .buf                   = &cl,
-        .name                  = "ikanaide",
-        .ext                   = "\0\0\0",
-        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = CLUSTER_SIZE,
-    };
-    int8_t retcode;
-    interrupt(0, (uint32_t) &request, (uint32_t) &retcode, 0);
-    if (retcode == 0)
-        interrupt(5, (uint32_t) "owo\n", 4, 0xF);
+size_t strlen(char *ptr) {
+    uint32_t i = 0;
+    while (ptr[i] != '\0')
+        i++;
+    return i;
+}
 
+void puts(char *buf, uint8_t color) {
+    interrupt(5, (uint32_t) buf, strlen(buf), color);
+}
+
+int main(void) {
     char buf[16];
     while (TRUE) {
+        puts("Brush@OS-IF2230", BIOS_LIGHT_GREEN);
+        puts(":", BIOS_GRAY);
+        puts("/", BIOS_LIGHT_BLUE);
+        puts("$ ", BIOS_GRAY);
         interrupt(4, (uint32_t) buf, 16, 0);
-        interrupt(5, (uint32_t) buf, 16, 0xF);
     }
 
     return 0;
