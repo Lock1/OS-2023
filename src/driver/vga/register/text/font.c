88e1eccd014e366586a1171993bd03934fc839b6
Lock1 <rizaltanur@outlook.com>
2023-04-09 22:35:16 +0700

    fix: VGA - Even-Odd & improper font copy

--- src/driver/vga/register/text/font.c
+++ src/driver/vga/register/text/font.c
@@ -6,7 +6,8 @@
 #include "lib-header/stdmem.h"
 
 void vga_load_standard_vga_font(void) {
-    // Graphics
+    // Need to change register to access font plane (plane number 2)
+    // Graphics register
     uint8_t graphics_register = vga_read_double_port_register(_vga_reg_port_graphics, GRAPHICS_INDEX_GRAPHICS_MODE);
     uint8_t misc_register     = vga_read_double_port_register(_vga_reg_port_graphics, GRAPHICS_INDEX_MISCELLANEOUS_GRAPHICS);
 
@@ -15,12 +16,13 @@ void vga_load_standard_vga_font(void) {
     memcpy(&temp_graphics_mode, &graphics_register, 1);
     memcpy(&temp_misc_graphics, &misc_register, 1);
     temp_graphics_mode.write_mode        = 0;
+    temp_graphics_mode.host_even_odd     = 0;
     temp_misc_graphics.chain_even_odd    = 0;
     temp_misc_graphics.memory_map_select = 1;
     vga_set_double_port_register(_vga_reg_port_graphics, GRAPHICS_INDEX_GRAPHICS_MODE, register_const_serialize(&temp_graphics_mode));
     vga_set_double_port_register(_vga_reg_port_graphics, GRAPHICS_INDEX_MISCELLANEOUS_GRAPHICS, register_const_serialize(&temp_misc_graphics));
 
-    // Sequencer
+    // Sequencer register
     uint8_t map_mask_register        = vga_read_double_port_register(_vga_reg_port_sequencer, SEQUENCER_INDEX_MAP_MASK);
     uint8_t memory_selector_register = vga_read_double_port_register(_vga_reg_port_sequencer, SEQUENCER_INDEX_MEMORY_MODE);
 
@@ -31,12 +33,12 @@ void vga_load_standard_vga_font(void) {
     vga_set_double_port_register(_vga_reg_port_sequencer, SEQUENCER_INDEX_MAP_MASK, register_const_serialize(&temp_map_mask));
     vga_set_double_port_register(_vga_reg_port_sequencer, SEQUENCER_INDEX_MEMORY_MODE, register_const_serialize(&temp_memory_mode));
 
-    // Font
-    // TODO : Set font
-    // memcpy((void*) VGA_LOWEST_FRAMEBUFFER_ADDRESS, bubach_font_8x16, 16);
-    // for (uint32_t i = 0; i < 256; i++)
-    //     memcpy((void*) VGA_LOWEST_FRAMEBUFFER_ADDRESS, bubach_font_8x16 + i*16, 16);
-
+    // Copying font
+    uint32_t font_address = VGA_LOWEST_FRAMEBUFFER_ADDRESS;
+    for (uint32_t i = 0; i < 256; i++) {
+        memcpy((void*) font_address, bubach_font_8x16 + i*16, 16);
+        font_address += 32; // Due every char is 32 bytes (first 16 bytes is bitmap), we need to jump per 32 bytes
+    }
 
     // Restore original register
     vga_set_double_port_register(_vga_reg_port_graphics, GRAPHICS_INDEX_GRAPHICS_MODE, graphics_register);
--- src/lib-header/driver/vga/register/text/font.h
+++ src/lib-header/driver/vga/register/text/font.h
@@ -4,6 +4,8 @@
 #include "lib-header/driver/vga/vga-register-programmer.h"
 #include "lib-header/stdtype.h"
 
+// https://wiki.osdev.org/VGA_Fonts
+// http://bos.asmhackers.net/docs/vga_without_bios/snippet_5/vga.php
 void vga_load_standard_vga_font(void);
 
 #endif
